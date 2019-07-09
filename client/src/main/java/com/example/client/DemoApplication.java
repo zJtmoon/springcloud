49d1f7194a46e528d48bc8020ed8a3d371939177
zJtmoon <850886237@qq.com>
2019-07-09 21:29:59 +0800

    修改

--- client/src/main/java/com/example/client/DemoApplication.java
+++ client/src/main/java/com/example/client/DemoApplication.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DemoApplication {
 
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
