23afd372941e88e7dc2855adbf6141ea9b985d90
zJtmoon <850886237@qq.com>
2019-11-06 23:55:17 +0800

    client

---
+++ server/src/main/java/com/example/server/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DemoApplication {
+    /**
+     * 解决resttemplete找不到的原因,如果使用rest方式使用别名的方式进行调用，需要依赖ribbon负载均衡器需要加@LoadBalance
+     * @LoadBalanced 能让restremplate在请求时拥有客户端的负载均衡的能力
+     * @param
+     */
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ server/src/main/java/com/example/server/controller/orderController.java
@@ -0,0 +1,35 @@
+package com.example.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zjt
+ * @create 2019-11-06-下午11:28
+ * @desc：
+ */
+@RestController
+public class orderController {
+    @Resource
+    private RestTemplate restTemplate;
+    /**
+     * 在springcloud。有两种方式http   1.rest是有springboot提供的采用httpclient技术 2.fegin它是springcloud的
+     * @return
+     */
+    //订单服务调用会员服务
+    @RequestMapping("/getOrder")
+    public String getOrder(){
+        //使用别名去注册中心上获取对应的服务调用地址
+        String url="http://hello-springcloud/getMember";
+//        //这样其实不对
+//       String result= restTemplate.getForObject("http://127.0.0.1:8081/getMember",String.class);
+
+        String result= restTemplate.getForObject(url,String.class);
+        System.out.println("订单服务调用会员服务的result："+result);
+         return result;
+    }
+}
