eaf14f40d16678b197bd7780cfab54dc72983b33
zJtmoon <850886237@qq.com>
2019-11-07 15:12:09 +0800

    修改

--- server/src/main/java/com/example/server/DemoApplication.java
+++ server/src/main/java/com/example/server/DemoApplication.java
@@ -12,16 +12,17 @@ import org.springframework.web.client.RestTemplate;
 public class DemoApplication {
     /**
      * 解决resttemplete找不到的原因,如果使用rest方式使用别名的方式进行调用，需要依赖ribbon负载均衡器需要加@LoadBalance
-     * @LoadBalanced 能让restremplate在请求时拥有客户端的负载均衡的能力
+     *
      * @param
+     * @LoadBalanced 能让restremplate在请求时拥有客户端的负载均衡的能力
      */
     @Bean
     @LoadBalanced
-    RestTemplate restTemplate(){
+    RestTemplate restTemplate() {
         return new RestTemplate();
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
     }
