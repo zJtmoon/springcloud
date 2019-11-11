a547d6297172253ba3a18a359b679048697271ff
zJtmoon <850886237@qq.com>
2019-11-11 18:49:11 +0800

    修改

--- server/src/main/java/com/example/server/controller/orderController.java
+++ server/src/main/java/com/example/server/controller/orderController.java
@@ -1,11 +1,8 @@
 package com.example.server.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import javax.annotation.Resource;
 
 /**
